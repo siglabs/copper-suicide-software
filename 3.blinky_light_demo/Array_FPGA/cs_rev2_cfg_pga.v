1a63a5210079ead0b056a910fb0de2506e758cd0
Mihir <mihir.utdallas@gmail.com>
2017-10-30 18:33:35 -0700

    led blink test for array and config fpga

---
+++ 3.blinky_light_demo/Array_FPGA/cs_rev2_cfg_pga.v
@@ -0,0 +1,17 @@
+module cs_rev2_cfg_fpga(
+							output wire       led,
+							input  wire       CLK
+						);
+
+
+assign led = counter[$bits(counter)-1];
+
+reg [23:0]counter=24'b0;
+
+always @(posedge CLK)
+begin
+	counter <= counter+1;
+end
+
+endmodule
+
